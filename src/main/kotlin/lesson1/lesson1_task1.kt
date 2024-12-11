0ddef87070eb40de470e2625993e2434c4a2a099
aka-i2gan <aka.i2gan@gmail.com>
2024-12-11 17:56:41 +0400

    KS-1-varables-1 solution

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -2,5 +2,5 @@ package org.example.lesson1
 
 fun main() {
     val numberOfOrders: Int = 75
-    val textWithThanks: String = "Thank you for your cooperation"
+    val textWithThanks: String = "Thanks for your cooperation"
 }
