757815acff025a73ff7c957d889d9df5612be4b4
aka-i2gan <aka.i2gan@gmail.com>
2024-12-11 19:07:36 +0400

    KS-1-varables-4 solution

---
+++ src/main/kotlin/lesson1/lesson1_task4.kt
@@ -0,0 +1,18 @@
+package org.example.lesson1
+
+fun main() {
+
+    // variables
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453e-4
+    val apogee: Int = 327000
+
+    val flightData: String = "$length\n$age\n$partOfDay\n$seconds\n$partOfYear\n$apogee"
+
+    // output of variables
+    println(flightData)
+
+}
