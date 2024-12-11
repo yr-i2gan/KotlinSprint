cd74ee536694e2600a2956f1104777fd54035660
aka-i2gan <aka.i2gan@gmail.com>
2024-12-11 18:14:43 +0400

    KS-1-varables-1 solution

---
+++ src/main/kotlin/lesson1/lesson2_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val textWithThanks: String = "Thanks for your cooperation"
+
+    println(numberOfOrders)
+    println(textWithThanks)
+
+    var numberOfEmployees: Int = 2000
+    // println(numberOfEmployees)
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}
